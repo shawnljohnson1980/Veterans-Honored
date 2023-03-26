6dfae919160fef057067ad04282d2351da0b963f
shawnljohnson1980 <shawnljohnson1980@yahoo.com>
2023-03-26 13:49:19 -0400

    initial commit for site

---
+++ veterans_honored/src/components/about.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../index.css";
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="mt-5">HONOR DIGNITY RESPECT</h2>
+      <br />
+      <div class="col-md-6">
+    <div class="p-xl-5 m-xl-5"><img class="rounded img-fluid w-100 fit-cover" style="min-height: 300px;" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png" /></div>
+</div>
+    </div>
+  );
+};
+
+export default About;
